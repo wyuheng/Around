e4a4b58a6ee530d3d9abab682e389a50cbe99c95
wyuheng <wyuheng@umich.edu>
2023-01-29 04:28:54 -0500

    backend upload

---
+++ constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://35.223.102.82:9200"
+	ES_USERNAME = "wyuheng"
+	ES_PASSWORD = "972609656"
+	GCS_BUCKET  = "wyuheng-bucket"
+)
