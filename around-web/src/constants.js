6cb3baa61b0ed3a0fd391c76d7ba233e8044dce0
wyuheng <78201459+wyuheng@users.noreply.github.com>
2023-02-03 17:40:37 -0500

    frontend upload

---
+++ around-web/src/constants.js
@@ -0,0 +1,9 @@
+export const TOKEN_KEY = 'token';
+export const BASE_URL = 'https://around-374118.uk.r.appspot.com';
+
+export const SEARCH_KEY = {
+    all: 0,
+    keyword: 1,
+    user: 2
+};
+   
